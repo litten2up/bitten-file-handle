18cb11a3d37d13b324045fe41a89376131b165a2
bitten2up <seant9140>
2022-11-16 13:30:31 -0600

    update

--- bit_file.c
+++ bit_file.c
@@ -7,15 +7,39 @@
 
 int readSave(){
 	FILE *f1 = fopen("bitten.sav", "rb"); // open in binary mode
+	char buffer[];
+	long saveSize;
 	if (f1!=NULL){
-		
-		return 0;
+		fseek(f1, 0L, SEEK_END);
+		saveSize = ftell(f1);
+		rewind(f1);
+		buffer = calloc(1, saveSize+1);
+		if (!buffer)
+		{
+			fclose(f1);
+			fputs("mem alloc failed", stderr);
+			return 1;
+		}
+		if (1!=fread(buffer,saveSize, 1, f1))
+		{
+			fclose(f1);
+			fputs("read failed", stderr);
+			return 1;
+		}
+		// read and printout the data
+		printf("Version of game saved: %c", buffer[1]);
+		// free memory
+		fclose(f1);
+		free(buffer);
 	} // dont write default data to file that already exsits
-	f1 = fopen("bitten.sav", "wb");
-	printf("save data that is being created\n");
-	printf("%s", saveD);
-	printf("\n");
-	fwrite(saveD, sizeof(char), sizeof(saveD), f1);
-	fclose(f1);
+	else
+	{
+		f1 = fopen("bitten.sav", "wb");
+		printf("save data that is being created\n");
+		printf("%s", saveD);
+		printf("\n");
+		fwrite(saveD, sizeof(char), sizeof(saveD), f1);
+		fclose(f1);
+	}
 	return 0;
 }
--- defaultSave.h
+++ defaultSave.h
@@ -1,6 +1,7 @@
 unsigned char saveD[] = {
-  'b', 'i', 't', 't', 0xf2, 'n', 'S', 0xf2, 'v', 'e', 0x0a, // header
-  0x01, // version
-  0x01 // first value: music
-};
-unsigned int test_txt_len = 13;
+  'b', 'i', 't', 't', 0xf2, 'n', 'S', 0xf2, 'v', 'e', // header
+  0x00, // version
+  0b100000, // first value: music
+  0x0a
+}; 
+unsigned int saveDlen = 13;
