c21b684ff934a0f4a405bd4c7af79dc27e52b75a
bitten2up <seant9140@gmail.com>
2022-11-16 10:44:40 -0600

    updated file

--- defaultSave.h
+++ defaultSave.h
@@ -1,5 +1,6 @@
 unsigned char saveD[] = {
   'b', 'i', 't', 't', 0xf2, 'n', 'S', 0xf2, 'v', 'e', 0x0a, // header
-  0x01 // version
+  0x01, // version
+  0x01 // first value: music
 };
-unsigned int test_txt_len = 12;
+unsigned int test_txt_len = 13;
