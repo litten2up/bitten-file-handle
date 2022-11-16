05675135d633298fa9659d02a34fe13bc320fc81
bitten2up <seant9140@gmail.com>
2022-11-16 12:43:35 -0600

    update abit

--- bit_file.c
+++ bit_file.c
@@ -3,12 +3,14 @@
 #include <stddef.h>
 #include "defaultSave.h"	// default savefile
 #include "bit_file.h"
-
-int parseSave(FILE* save);
+#define VERSION 0x01
 
 int readSave(){
 	FILE *f1 = fopen("bitten.sav", "rb"); // open in binary mode
-	if (f1!=NULL){return 0;} // dont write default data to file that already exsits
+	if (f1!=NULL){
+		
+		return 0;
+	} // dont write default data to file that already exsits
 	f1 = fopen("bitten.sav", "wb");
 	printf("save data that is being created\n");
 	printf("%s", saveD);
@@ -16,10 +18,4 @@ int readSave(){
 	fwrite(saveD, sizeof(char), sizeof(saveD), f1);
 	fclose(f1);
 	return 0;
-}
-#define VERSION 0x01
-// where the magic happens
-int parseSave(FILE* save)
-{
-	
 }
